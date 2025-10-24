daf5bd66da0414e0603a7fc1128a6b5c637ccd06
drakshall <149804101+drakshall@users.noreply.github.com>
2025-10-24 12:43:36 +0100

    Update LinkedList.cpp

--- Code and such/LinkedList.cpp
+++ Code and such/LinkedList.cpp
@@ -4,52 +4,57 @@
 class DoublyLinkedList{
     private:
 
-        struct LinkedNode{                              // Defines node structure
+        struct LinkedNode{                                                          // defines node structure
             std::string arbitraryData;
             LinkedNode* next;
             LinkedNode* prev;
         };
 
-        LinkedNode* head;                               // Defines the head & tail pointers
+        LinkedNode* head;                                                           // defines the head & tail pointers
         LinkedNode* tail;
 
     public:
 
-        DoublyLinkedList(){                             // Initialises head & tail to nullptr to avoid undefined behaviour
+        DoublyLinkedList(){                                                         // initialises head & tail to nullptr to avoid undefined behaviour
             head = nullptr;
             tail = nullptr;
         };
 
-        void Prepend(std::string inputData){            // class method to prepend a node to the start of the LL
-            LinkedNode* temp = new LinkedNode;          // creates new node and points temp to its memory address
-            temp -> arbitraryData = inputData;          // takes the data input and writes it to the new node
-            temp -> next = head;                        // points the new node's 'next' pointer to the old head node
-            temp -> prev = nullptr;                     // this node will be the new head so there's no previous node to point to
+        void Prepend(std::string inputData){                                        // class method to prepend a node to the start of the LL
+            LinkedNode* tempPointer = new LinkedNode;                               // creates new node and temporarily points to its memory address
+            tempPointer -> arbitraryData = inputData;                               // takes the data input and writes it to the new node
+            tempPointer -> next = head;                                             // points the new node's 'next' pointer to the old head node
+            tempPointer -> prev = nullptr;                                          // this node will be the new head so there's no previous node to point to
 
-            if (head != nullptr){                       // checks if the LL is empty 
-                head -> prev = temp;                    // if the LL already has entries this points the old head node's prev pointer to the new node
+            if (head != nullptr){                                                   // checks if the LL is empty 
+                head -> prev = tempPointer;                                         // if the LL already has entries this points the old head node's prev pointer to the new node
             }
             else{
-                tail = temp;                            // if the LL is empty then the new node becomes both head & tail of the LL
+                tail = tempPointer;                                                 // if the LL is empty then the new node becomes both head & tail of the LL
             };
 
-            head = temp;                                // points the head pointer at the new node
+            head = tempPointer;                                                     // points the head pointer at the new node
         };
 
-        void append(std::string inputData){             // class method to append a node to the end of the LL
-            LinkedNode* temp = new LinkedNode;          // creates new node and points temp to its memory address
-            temp -> arbitraryData = inputData;          // takes the data input and writes it to the new node
-            temp -> next = nullptr;                     // this node will be the new tail so there's no next node to point to
-            temp -> prev = tail;                        // points the new node's 'prev' pointer to the old tail node
+        void Append(std::string inputData){                                         // class method to append a node to the end of the LL
+            LinkedNode* tempPointer = new LinkedNode;                               // creates new node and temporarily points to its memory address
+            tempPointer -> arbitraryData = inputData;                               // takes the data input and writes it to the new node
+            tempPointer -> next = nullptr;                                          // this node will be the new tail so there's no next node to point to
+            tempPointer -> prev = tail;                                             // points the new node's 'prev' pointer to the old tail node
 
-            if (tail != nullptr){                       // checks if the LL is empty 
-                tail -> next = temp;                    // if the LL already has entries this points the old tail node's next pointer to the new node
+            if (tail != nullptr){                                                   // checks if the LL is empty 
+                tail -> next = tempPointer;                                         // if the LL already has entries this points the old tail node's next pointer to the new node
             }
             else{
-                head = temp;                            // if the LL is empty then the new node becomes both head & tail of the LL
+                head = tempPointer;                                                 // if the LL is empty then the new node becomes both head & tail of the LL
             };
 
-            tail = temp;                                // points the tail pointer to the new node
+            tail = tempPointer;                                                     // points the tail pointer to the new node
+        };
+
+        void Insert(std::string inputData, int tempIndex, bool forwardBackward){    // method to insert a node at a specific position in the LL
+            LinkedNode* tempPointer = new LinkedNode;                               // creates new node and temporarily points to its memory address
+
         };
 };
 
