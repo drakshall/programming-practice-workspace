fa58efeb4683e8c64235e20b0cbf1597caf734d3
drakshall <149804101+drakshall@users.noreply.github.com>
2026-05-06 21:16:36 +0100

    kivy sequence game project

--- sequencegame.py
+++ sequencegame.py
@@ -2,6 +2,11 @@ from kivy.config import Config
 Config.set('graphics', 'fullscreen', 'auto')
 
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+class InputNameForm(BoxLayout):
+    def nameInputHandler(self):
+        print("hi :)")
 
 class SequenceApp(App):
     pass
